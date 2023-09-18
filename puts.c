e517fece1f535cebb0e9fe9c804d7527e5b7fcee
oussama alx linux <oussama11chaouki@gmail.com>
2023-09-18 18:12:29 +0100

    task 01

--- puts.c
+++ puts.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
- * _puts - prints a string, followed by a new line
- * @str:the string
- * Return:size of string
- */
-int _puts(char *str)
+* _puts - prints a string, followed by a new line
+* @str:the string
+* Return:size of string
+*/
+int _puts(const char *str)
 {
 	int size = 0;
 
 	if (str == NULL)
-		str = "(null)";
+	str = "(null)";
 
 	while (str[size] != '\0')
 		size++;
+
 	return (write(1, str, size));
 }
