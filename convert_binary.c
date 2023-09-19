854913b42d2aea331f466df0206c23ec0efa6034
Gift Obia <gyvfted3@gmail.com>
2023-09-19 06:14:04 -0700

    convert_binary.c

--- convert_binary.c
+++ convert_binary.c
@@ -1,82 +1,64 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "main.h"
 
 /**
- * print_binary - Custom function to convert an unsigned integer to binary.
- * @num: The unsigned integer to be converted and printed.
- */
-void print_binary(unsigned int num)
-{
-    if (num == 0)
-    {
-        putchar('0');
-        return;
-    }
-
-    int binary[32]; // Assuming 32-bit integers
-    int index = 0;
-
-    while (num > 0)
-    {
-        binary[index++] = num % 2;
-        num /= 2;
-    }
-
-    // Print binary representation in reverse order
-    for (int i = index - 1; i >= 0; i--)
-    {
-        putchar(binary[i] + '0');
-    }
-}
-
-/**
- * custom_printf - Custom printf-like function with %b specifier.
- * @format: The format string with custom specifiers.
- * @...: Variable number of arguments to match format specifiers.
- */
-void custom_printf(const char *format, ...)
-{
-    va_list args;
-    va_start(args, format);
-
-    char c;
-    while ((c = *format))
-    {
-        if (c != '%')
-        {
-            putchar(c);
-        }
-        else
-        {
-            format++; // Move past '%'
-            if (*format == 'b')
-            {
-                unsigned int num = va_arg(args, unsigned int);
-                print_binary(num);
-            }
-            else
-            {
-                putchar('%'); // Handle other % cases
-            }
-        }
-        format++;
-    }
-
-    va_end(args);
-}
-
-/**
- * main - Entry point
+ * _to_binary - Convert an unsigned int to binary
+ * @n: The unsigned int to convert
  *
- * Return: Always 0
+ * Return: The number of characters printed
  */
-int main()
+int _to_binary(unsigned int n)
 {
-    unsigned int num = 50; // Example unsigned integer
-    custom_printf("Binary representation of %b is: ", num);
-    putchar('\n');
+	int count = 0;
+	unsigned int mask = 1 << (sizeof(unsigned int) * 8 - 1);
 
-    return 0;
+	while (mask > 0)
+	{
+		if (n & mask)
+			_putchar('1');
+		else
+			_putchar('0');
+		count++;
+		mask >>= 1;
+	}
+	return (count);
 }
 
+/**
+ * custom_printf - Custom printf function
+ * @format: The format string
+ * @...: Additional arguments
+ *
+ * Return: The number of characters printed
+ */
+int custom_printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'b')
+			{
+				unsigned int n = va_arg(args, unsigned int);
+				count += _to_binary(n);
+			}
+			else
+			{
+				_putchar('%');
+				count++;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
